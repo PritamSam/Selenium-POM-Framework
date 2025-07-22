ece3341f3d52bc94eee2c5a36ac5e007e8c6f18b
PritamSam <Pritamsamantabond007@gmail.com>
2025-07-22 19:10:28 +0530

    Initial commit

---
+++ Selenium-Locators/src/main/java/Sample.java
@@ -0,0 +1,17 @@
+/*
+ *  src/main/java folder is used for experimenting Selenium code. It is not intended to write Test Code 
+ *  It is used to write application code
+ *  
+ * */
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello My name is Pritam samanta");
+
+	}
+
+}
